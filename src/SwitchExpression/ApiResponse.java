ba993387db388b354788ff419ce4fc824c79e407
Faisal Ahsan <f.ahsan.07@gmail.com>
2023-05-14 18:04:38 +0100

    Refactor API response to be a record

--- src/SwitchExpression/ApiResponse.java
+++ src/SwitchExpression/ApiResponse.java
@@ -1,25 +1,3 @@
 package SwitchExpression;
 
-public class ApiResponse {
-    private final Status status;
-    private final String message;
-    private final Object data;
-
-    public ApiResponse(Status status, String message, Object data) {
-        this.status = status;
-        this.message = message;
-        this.data = data;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public Object getData() {
-        return data;
-    }
-}
+public record ApiResponse (Status status, String message, Object data) { }
